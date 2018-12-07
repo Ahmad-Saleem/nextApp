cc07d8b3f9109173f976010ddef108aae5091976
Ahmad Saleem <it_saleem@hotmail.com>
2018-12-07 19:43:10 +0000

    use custom express server

--- server.js
+++ server.js
@@ -1,34 +1,29 @@
-const { createServer } = require('http')
-const { parse } = require('url')
+const express = require('express')
 const next = require('next')
-const pathMatch = require('path-match')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const route = pathMatch()
-const match = route('/about/:x')
 
 app.prepare()
   .then(() => {
-    createServer((req, res) => {
+    const server = express()
 
-        
-    const { pathname, query } = parse(req.url, true)
-
-    const params = match(pathname)
-    //console.log('params: ', params)
-    if (params === false) { 
-        handle(req, res)
-        return
-    }
-    //console.log({pathname, query})
-    app.render(req, res, '/about', Object.assign(params, query))
-
+    server.get('/about/:x', (req, res) => {
+      return app.render(req, res, '/about', {x: req.params.x})
     })
-      .listen(port, (err) => {
-        if (err) throw err
-        console.log(`> Ready on http://localhost:${port}`)
-      })
+
+    server.get('/b', (req, res) => {
+      return app.render(req, res, '/a', req.query)
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
   })
