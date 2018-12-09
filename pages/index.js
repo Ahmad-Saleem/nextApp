e081026831467c999f54683dc5eeab2fca74d826
Ahmad Saleem <it_saleem@hotmail.com>
2018-12-09 21:45:24 +0000

    Next, with Redux and Persist

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,14 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import Link from '../components/Link'
-class Home extends React.Component {
+import React from "react";
+import {connect} from "react-redux";
+import {setClientState } from '../store/persist';
 
-  // static async getInitialProps({ req, query }) {
-  //   console.log('query:',  query)
-  //   const q = query || "";
-  //   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-  //   return query
-  // }
-
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        Home Page {this.props.query},  <br />
-        <Link page='user' params='login' query='?op=login'>
-          login
-        </Link>
-      </div>
-    )
-  }
-}
-
-const mapStateToProps = ({user}) => {
-  return {user};
-}
-export default connect(mapStateToProps)(Home);
+export default connect(
+    (state) => state,
+    {setClientState}
+)(({fromServer, fromClient, setClientState}) => (
+    <div>
+        <div>fromServer: {fromServer}</div>
+        <div>fromClient: {fromClient}</div>
+        <div><button onClick={e => setClientState('bar')}>Set Client State</button></div>
+    </div>
+));
